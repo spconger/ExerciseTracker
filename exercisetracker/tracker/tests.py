1df79ddd949cd3e809c7af1447258f19620121c2
spconger <spconger@gmail.com>
2022-03-17 21:10:36 -0700

    commit 2 on tests

--- exercisetracker/tracker/tests.py
+++ exercisetracker/tracker/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Goal, Exercise
+from .models import Goal, Exercise, DailyExercise, CaloryCount, Weight
 import datetime
 
 # Tests for models
@@ -28,9 +28,59 @@ class TestGoal(TestCase):
 class TestExercise(TestCase):
     def setUp(self):
         entry=datetime.date(2022,2,22)
-        self.ex=Exercise(excercisename='pushups',measure='reps',dateentered=entry)
+        self.ex=Exercise(exercisename='pushups',measure='reps',dateentered=entry)
 
     def test_String(self):
-        self.assertEqual(str(self.ex, 'pushups'))
+        self.assertEqual(str(self.ex), 'pushups')
+
+    def test_measure(self):
+        self.assertEqual(self.ex.measure, 'reps')
+
+    def test_exerciseDate(self):
+        self.assertEqual(str(self.ex.dateentered), '2022-02-22')
+
+class TestDailyExercise(TestCase):
+    def setUp(self):
+        d=datetime.date(2022,3,10)
+        self.goal=Goal(goalname="test goal")
+        self.exercise=Exercise(exercisename="walking", measure='steps')
+        self.daily=DailyExercise(exercisedate=d, exercise=self.exercise, score=1000, goal=self.goal )
 
 
+    def test_exerciseString(self):
+        self.assertEqual(str(self.daily), '2022-03-10')
+
+    def test_exercise(self):
+        self.assertEqual(str(self.daily.exercise), 'walking')
+
+    def test_score(self):
+        self.assertEqual(self.daily.score, 1000)
+
+    def test_goal(self):
+        self.assertEqual(str(self.daily.goal), 'test goal')
+
+class TestCaloryCOunt(TestCase):
+    def setUp(self):
+        self.goal=Goal(goalname='test')
+        date=datetime.date(2022,3,10)
+        self.cal=CaloryCount(entrydate=date, calories=1500, goal=self.goal)
+
+    def test_string(self):
+        self.assertEqual(str(self.cal), '2022-03-10')
+
+    def test_date(self):
+        self.assertEqual(self.cal.entrydate, datetime.date(2022,3,10))
+
+    def test_goal(self):
+        self.assertEqual(str(self.cal.goal), 'test')
+
+#tests for non model classes in model
+class TestWeight(TestCase):
+    def setUp(self):
+        date=datetime.date(2022, 3,12)
+        self.goal=Goal(goalname="loose weight")
+        self.weight=Weight(weightdate=date, weightamt=190, goal=self.goal)
+    
+
+    def test_difference(self):
+        self.assertEqual(self.weight.distanceFromGoal(190), 20)
